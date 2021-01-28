debad0ea636a7ff39514916dee2b78d3721c3233
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 09:48:15 -0800

    demonstrate the "as" keyword to distinguish between two items of the same name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
-use std::fmt;
-use std::io;
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-fn function1() -> fmt::Result {
+fn function1() -> Result {
     // --snip--
 }
 
-fn function2() -> io::Result {
+fn function2() -> IoResult {
     // --snip--
 }
 
