0a6fb7239531c6f6d5bef0b296b64108567c8b37
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 09:26:18 -0800

    add breakfast struct as example of public structs with mixed privacy fields

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,20 @@ mod front_of_house {
 }
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         // here, we use super because we expect the structure between
@@ -49,4 +63,12 @@ fn eat_at_restaurant() {
 
     // relative path
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like to change it to {} toast please!", meal.toast);
+
+    // next line is invalid because seasonal fruit is private
+    // meal.seasonal_fruit = String::from("blueberries");
 }
