d6eebd3e4fb5abdf3182ce501a0a30ced952ac8c
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-28 09:47:30 -0800

    introduce two items with the same name to demonstrate need to scope the parent

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 use std::collections::HashMap;
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {
+    // --snip--
+}
+
+fn function2() -> io::Result {
+    // --snip--
+}
 
 fn main() {
     let mut map = HashMap::new();
